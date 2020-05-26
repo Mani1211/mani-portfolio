1a834f137d1125cb82d8a4c9db633c4d5cf718a0
Mani1211 <smanivasagamtnj@gmail.com>
2020-05-26 16:36:30 +0530

    .

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -57,7 +57,7 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.","love to learn new things.", "am aSelf learner", "solve problems."
+				"am a Web Developer.","love to learn new things.", "am aSelf learner", "solve problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
